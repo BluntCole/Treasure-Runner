0cf24c0c22e9b94c1e92d57b195a653276eb4bda
ColeBlunt <65433288+ColeBlunt@users.noreply.github.com>
2023-02-20 13:54:12 -0500

    displaying the map

--- GameEngine.py
+++ GameEngine.py
@@ -1,13 +1,39 @@
 import pygame
+import pytmx
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self,pos,surf,groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft = pos)
+
+pygame.init()
+screen = pygame.display.set_mode((1280,920))
+tiled_map = pytmx.util_pygame.load_pygame("GameMap/GameMap.tmx")
+sprite_group = pygame.sprite.Group()
+
+for layer in tiled_map.layers:
+    if layer.name in ('Physical for player and ball', 'physical for ball', 'physical for player'):
+        for x, y, surf in layer.tiles():
+            pos = (x * tiled_map.tilewidth, y * tiled_map.tileheight)
+            Tile(pos = pos, surf = surf, groups = sprite_group)
+
+# for layer in tiled_map.visible_layers:
+#     if isinstance(layer, pytmx.TiledTileLayer):
+#         for x, y, gid in layer:
+#             tile = tiled_map.get_tile_image_by_gid(gid)
+#             screen.blit(tile, (x * tiled_map.tilewidth, y * tiled_map.tileheight))
 
 class GameEngine:
-    def __init__(self, screen_size=(640, 480), fps=30):
+    def __init__(self, screen_size=(700, 700), fps=30):
         pygame.init()
         self.screen = pygame.display.set_mode(screen_size)
         self.clock = pygame.time.Clock()
         self.fps = fps
         self.running = False
         self.game_objects = []
+
+
 
     def add_game_object(self, game_object):
         self.game_objects.append(game_object)
@@ -25,15 +51,20 @@ class GameEngine:
                     self.running = False
 
             # Update game objects
-            for game_object in self.game_objects:
-                game_object.update()
+            #for game_object in self.game_objects:
+                #game_object.update()
 
-            # Clear screen
-            self.screen.fill((0, 0, 0))
+            sprite_group.draw(screen)
+                # Draw Tiled map
+            # for layer in self.tiled_map.visible_layers:
+            #     if isinstance(layer, pytmx.TiledTileLayer):
+            #         for x, y, gid in layer:
+            #             tile = self.tiled_map.get_tile_image_by_gid(gid)
+            #             self.screen.blit(tile, (x * self.tiled_map.tilewidth, y * self.tiled_map.tileheight))
 
             # Draw game objects
-            for game_object in self.game_objects:
-                game_object.draw(self.screen)
+            #for game_object in self.game_objects:
+                #game_object.draw(self.screen)
 
             # Update display
             pygame.display.flip()
